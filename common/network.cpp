7b8f98d8fde5ce63437261ba91efa9610afe213b
Duck Ran <1696095803@qq.com>
2025-11-19 17:06:19 +0800

    Change: Using the LogHelper

--- common/network.cpp
+++ common/network.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
-#include<sqlite3.h>
+#include <iostream>
+#include <sqlite3.h>
 #include "network.hpp"
-#include"../third_party/json.hpp"
+#include"../common/log_helper.hpp"
+#include "../third_party/json.hpp"
 
 using namespace std;
 using nlohmann::json;
@@ -12,9 +13,15 @@ void send_packet(int socket, const ClientPacket &packet)
     string json_string = serialized.dump();
 
     uint32_t length = htonl(json_string.length());
-    send(socket, &length, sizeof(length), 0);
-
-    send(socket, json_string.c_str(), json_string.length(), 0);
+    try
+    {
+        send(socket, &length, sizeof(length), 0);
+        send(socket, json_string.c_str(), json_string.length(), 0);
+    }
+    catch (const exception &e)
+    {
+        log(LogLevel::ERROR, e.what());
+    }
 }
 
 void send_packet(int socket, const ServerPacket &packet)
@@ -23,12 +30,19 @@ void send_packet(int socket, const ServerPacket &packet)
     string json_string = serialized.dump();
 
     uint32_t length = htonl(json_string.length());
-    send(socket, &length, sizeof(length), 0);
-
-    send(socket, json_string.c_str(), json_string.length(), 0);
+    try
+    {
+        send(socket, &length, sizeof(length), 0);
+        send(socket, json_string.c_str(), json_string.length(), 0);
+    }
+    catch (const exception &e)
+    {
+        log(LogLevel::ERROR, e.what());
+    }
 }
 
-ClientPacket recv_client_packet(int socket){
+ClientPacket recv_client_packet(int socket)
+{
     uint32_t net_length;
     recv(socket, &net_length, sizeof(uint32_t), 0);
     uint32_t host_length = ntohl(net_length);
@@ -36,11 +50,12 @@ ClientPacket recv_client_packet(int socket){
     string json_string;
     json_string.resize(host_length);
     recv(socket, &json_string[0], host_length, 0);
-    
+    log(LogLevel::INFO, "Received client packet.");
     return json::parse(json_string).get<ClientPacket>();
 }
 
-ServerPacket recv_server_packet(int socket){
+ServerPacket recv_server_packet(int socket)
+{
     uint32_t net_length;
     recv(socket, &net_length, sizeof(uint32_t), 0);
     uint32_t host_length = ntohl(net_length);
@@ -48,6 +63,6 @@ ServerPacket recv_server_packet(int socket){
     string json_string;
     json_string.resize(host_length);
     recv(socket, &json_string[0], host_length, 0);
-    
+    log(LogLevel::INFO, "Received server packet.");
     return json::parse(json_string).get<ServerPacket>();
 }
