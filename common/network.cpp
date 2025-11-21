f8072e2936ea657b05ccbd321eb8f0f025c8396a
Duck Ran <1696095803@qq.com>
2025-11-21 14:14:41 +0800

    Fix: Improve network packet handling with error checking

--- common/network.cpp
+++ common/network.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sqlite3.h>
 #include "network.hpp"
-#include"../common/log_helper.hpp"
+#include "../common/log_helper.hpp"
 #include "../third_party/json.hpp"
 
 using namespace std;
@@ -44,25 +44,88 @@ void send_packet(int socket, const ServerPacket &packet)
 ClientPacket recv_client_packet(int socket)
 {
     uint32_t net_length;
-    recv(socket, &net_length, sizeof(uint32_t), 0);
+    int result = recv(socket, &net_length, sizeof(uint32_t), 0);
+    if (result != sizeof(uint32_t))
+    {
+        log(LogLevel::ERROR, "Failed to receive packet length");
+        return ClientPacket();
+    }
     uint32_t host_length = ntohl(net_length);
+
+    const uint32_t MAX_PACKET_SIZE = 1024 * 1024;
+    if (host_length > MAX_PACKET_SIZE)
+    {
+        log(LogLevel::ERROR, "Packet too large: " + to_string(host_length));
+        return ClientPacket();
+    }
 
     string json_string;
     json_string.resize(host_length);
-    recv(socket, &json_string[0], host_length, 0);
+    result = recv(socket, &json_string[0], host_length, 0);
+    if (result == -1)
+        return ClientPacket();
     log(LogLevel::INFO, "Received client packet.");
-    return json::parse(json_string).get<ClientPacket>();
+
+    ClientPacket packet;
+    try
+    {
+        packet = json::parse(json_string).get<ClientPacket>();
+    }
+    catch (exception e)
+    {
+        log(LogLevel::ERROR, e.what());
+        return ClientPacket();
+    }
+    return packet;
 }
 
 ServerPacket recv_server_packet(int socket)
 {
     uint32_t net_length;
-    recv(socket, &net_length, sizeof(uint32_t), 0);
+    int result = recv(socket, &net_length, sizeof(uint32_t), 0);
+    if (result != sizeof(uint32_t))
+    {
+        log(LogLevel::ERROR, "Failed to receive packet length");
+        return ServerPacket();
+    }
     uint32_t host_length = ntohl(net_length);
+
+    const uint32_t MAX_PACKET_SIZE = 1024 * 1024;
+    if (host_length > MAX_PACKET_SIZE)
+    {
+        log(LogLevel::ERROR, "Packet too large: " + to_string(host_length));
+        return ServerPacket();
+    }
 
     string json_string;
     json_string.resize(host_length);
-    recv(socket, &json_string[0], host_length, 0);
+    result = recv(socket, &json_string[0], host_length, 0);
+    if (result == -1)
+        return ServerPacket();
     log(LogLevel::INFO, "Received server packet.");
-    return json::parse(json_string).get<ServerPacket>();
+
+    ServerPacket packet;
+    try
+    {
+        packet = json::parse(json_string).get<ServerPacket>();
+    }
+    catch (exception e)
+    {
+        log(LogLevel::ERROR, e.what());
+        return ServerPacket();
+    }
+    return packet;
+}
+
+bool check_online(int socket)
+{
+    int error = 0;
+    socklen_t len = sizeof(error);
+    int result = getsockopt(socket, SOL_SOCKET, SO_ERROR, &error, &len);
+
+    if (result != 0 || error != 0)
+    {
+        return false;
+    }
+    return true;
 }
--- common/network.hpp
+++ common/network.hpp
@@ -9,3 +9,4 @@ void send_packet(int socket, const ClientPacket &client_packet);
 void send_packet(int socket, const ServerPacket &server_packet);
 ClientPacket recv_client_packet(int socket);
 ServerPacket recv_server_packet(int socket);
+bool check_online(int socket);
